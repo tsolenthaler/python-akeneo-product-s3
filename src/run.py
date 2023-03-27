925e04048e1826c505ec373442b006795bf81f0f
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-03-27 16:43:32 +0200

    test commit

--- src/run.py
+++ src/run.py
@@ -32,7 +32,12 @@ def __main__():
     print(f"Arguments count: {len(sys.argv)}")
     for i, arg in enumerate(sys.argv):
         print(f"Argument {i:>6}: {arg}")
+    # Extract
     product = getProductFromAkeneo()
+    # Transform
+    # none
+    
+    # Load
     dictToS3(product, S3_BUCKET, S3_OBJECT_EXPORT_PATH)
 
 if __name__== "__main__":
