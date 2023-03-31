e5310f7d4038eb8eb9bb719fad89984c3dde7a63
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-03-31 10:04:03 +0200

    fix naming

--- src/createIndexbyId.py
+++ src/createIndexbyId.py
@@ -10,12 +10,12 @@ S3_BUCKET = getenv('S3_BUCKET')
 S3_REGION = getenv('S3_REGION')
 S3_ACCESS_KEY = getenv('S3_ACCESS_KEY')
 S3_SECRET_ACCESS_KEY = getenv('S3_SECRET_ACCESS_KEY')
+S3_OBJECT_PRODUCTS_INDEX = getenv('S3_OBJECT_PRODUCTS_INDEX')
 S3_OBJECT_PRODUCTS_INDEX_PATH = getenv('S3_OBJECT_PRODUCTS_INDEX_PATH')
-S3_OBJECT_PRODUCTS_INDEX_EXPORT_PATH = getenv('S3_OBJECT_PRODUCTS_INDEX_EXPORT_PATH')
 
 def getIndex():
     print("Get Index")
-    products = getObject(S3_BUCKET, S3_OBJECT_PRODUCTS_INDEX_PATH)
+    products = getObject(S3_BUCKET, S3_OBJECT_PRODUCTS_INDEX)
     return products
 
 def createIdIndex(products):
@@ -29,7 +29,7 @@ def __main__():
     print("Start Create Index")
     products = getIndex()
     index = createIdIndex(products)
-    dictToS3(index, S3_BUCKET, S3_OBJECT_PRODUCTS_INDEX_EXPORT_PATH+"id.json")
+    dictToS3(index, S3_BUCKET, S3_OBJECT_PRODUCTS_INDEX_PATH+"id.json")
 
 if __name__== "__main__":
     __main__()
